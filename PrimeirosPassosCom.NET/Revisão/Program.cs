05ceaac0b5ee88747cf73c54b59a96884a423e07
Everson dos Santos Pinheiro <79765901+everson-s@users.noreply.github.com>
2022-05-29 18:33:31 -0300

    Criação de exemplos para curso

---
+++ PrimeirosPassosCom.NET/Revisão/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Revisão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario =  Console.ReadLine();  
+
+            while(opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: Adicionar aluno
+
+                        break;
+
+                    case "2":
+                        //TODO: Listar alunos
+
+                        break;
+
+                    case "3":
+                        //TODO: Calcular média geral
+
+                        break;
+
+                    default:
+
+                        trow new ArgumentOutOfRangeException();
+                }
+
+                Console.WriteLine("Informe a opção desejada: ");
+                Console.WriteLine("1 - Inserir novo aluno");
+                Console.WriteLine("2 - Listar alunos");
+                Console.WriteLine("3 - Calcular média geral");
+                Console.WriteLine("X - Sair");
+                Console.WriteLine();
+
+                opcaoUsuario =  Console.ReadLine();
+            }
+        }
+    }
+}
