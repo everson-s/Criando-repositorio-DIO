a0631687bf691fe6974c817fb2687f4b59b4809c
Everson dos Santos Pinheiro <79765901+everson-s@users.noreply.github.com>
2022-05-30 00:25:17 -0300

    Atualizando a inserção de alunos

--- PrimeirosPassosCom.NET/Revisão/Program.cs
+++ PrimeirosPassosCom.NET/Revisão/Program.cs
@@ -4,9 +4,13 @@ namespace Revisão
 {
     class Program
     {
+                    
         static void Main(string[] args)
         {
-            
+           
+           Aluno [] alunos = new Aluno[5];
+           var indiceAluno = 0;
+
             string opcaoUsuario = ObterOpcaoUsuario();
 
             while(opcaoUsuario.ToUpper() != "X")
@@ -14,8 +18,24 @@ namespace Revisão
                 switch (opcaoUsuario)
                 {
                     case "1":
-                        //TODO: Adicionar aluno
+                        Console.WriteLine("Informe o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
 
+                        Console.WriteLine("Informe a nota do aluno: ");
+
+                        if(decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                        
                         break;
 
                     case "2":
@@ -28,28 +48,29 @@ namespace Revisão
 
                         break;
 
-                    default:
+                    // default:
 
-                        trow new ArgumentOutOfRangeException();
+                    // trow new ArgumentOutOfRangeException();
                 }
 
                 opcaoUsuario =  ObterOpcaoUsuario();
             }
 
-            private static string ObterOpcaoUsuario()
-            {
-                Console.WriteLine("Informe a opção desejada: ");
-                Console.WriteLine("1 - Inserir novo aluno");
-                Console.WriteLine("2 - Listar alunos");
-                Console.WriteLine("3 - Calcular média geral");
-                Console.WriteLine("X - Sair");
-                Console.WriteLine();
-
-                string opcaoUsuario =  Console.ReadLine(); 
-                return opcaoUsuario;
-            }
+            
         }
 
-        
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario =  Console.ReadLine(); 
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
     }
 }
