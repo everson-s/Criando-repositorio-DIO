055a1616a7755accd452171d7d3bb319e81f9803
Everson dos Santos Pinheiro <79765901+everson-s@users.noreply.github.com>
2022-05-30 12:42:42 -0300

    Somando dois números inteiros

---
+++ Tipos_de_referencia_e_valor_em_C#/Demos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace Demos
+{
+    class Program
+    {
+        static int AdicionarDoisValores(int valor1, int valor2)
+        {
+            int soma = 0;
+            soma = valor1 + valor2;
+
+            return soma;
+        }
+        static void Main(string[] args)
+        {
+            int primeiroValor = 0;
+            int segundoValor = 0;
+
+            WriteLine();
+            WriteLine("----- SOMANDO DOIS NÚMEROS INTEIROS -----");
+            WriteLine();
+            Write("Digite o primeiro valor..: ");
+            primeiroValor = Convert.ToInt32(ReadLine());
+            Write("Digite o segundo valor...: ");
+            segundoValor = Convert.ToInt32(ReadLine());
+            WriteLine();
+            WriteLine($"A soma entre {primeiroValor} e {segundoValor} é {AdicionarDoisValores(primeiroValor,segundoValor)}");
+
+            //Console.ReadKey();
+        }
+    }
+}
